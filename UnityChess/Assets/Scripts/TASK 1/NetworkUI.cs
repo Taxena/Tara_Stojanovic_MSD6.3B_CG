35319ceb85b73b1e4b151589597ecd773d2bdec1
Taxena <tara.stojanovic.e22239@mcast.edu.mt>
2025-03-28 18:46:02 +0100

    ParrelSync and Host/Client Logic

--- UnityChess/Assets/Scripts/TASK 1/NetworkUI.cs
+++ UnityChess/Assets/Scripts/TASK 1/NetworkUI.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
 using Unity.Netcode.Transports.UTP;
+using System;
 
 public class NetworkUI : MonoBehaviour
 {
@@ -14,32 +15,51 @@ public class NetworkUI : MonoBehaviour
 
     private void Awake()
     {
-        
+        JoinButton.onClick.AddListener(JoinGame);
+        RejoinButton.onClick.AddListener(RejoinGame);
+        HostButton.onClick.AddListener(HostGame);
+        LeaveButton.onClick.AddListener(LeaveGame);
     }
 
     private void JoinGame()
     {
-
+        Debug.Log("CLIENT JOINED THE GAME");
+        NetworkManager.Singleton.StartClient();
     }
 
     private void RejoinGame()
     {
-
+        throw new NotImplementedException();
     }
 
     private void HostGame()
     {
-
+        NetworkManager.Singleton.StartHost();
+        Debug.Log("GAME IS HOSTED");
+        //Debug.Log($"Server started listening on {transport.ConnectionData.ServerListenAddress} and port {transport.ConnectionData.Port}");
+        CheckIfRunningLocally();
     }
 
     private void LeaveGame()
     {
+        throw new NotImplementedException();
+    }
 
+    private void CheckIfRunningLocally()
+    {
+        if (transport.ConnectionData.ServerListenAddress == "127.0.0.1")
+        {
+            Debug.LogWarning("Server is listening locally (127.0.0.1) ONLY!");
+        }
     }
 
     private void Start()
     {
         transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
 
+        if (NetworkManager.Singleton != null)
+        {
+            Debug.Log($"UTP working with IP:{transport.ConnectionData.Address} and Port:{transport.ConnectionData.Port}");
+        }
     }
 }
